4f522be752af3099e1fd978ba3fe8eefd37fbbfc
JeongDay <cidd1332@gmail.com>
2025-11-13 12:30:42 +0900

    2025.11.13 목요일 오전시간2

--- Assets/_Practice/02. Scripts/Sprite Animation/CharacterMovement2D.cs
+++ Assets/_Practice/02. Scripts/Sprite Animation/CharacterMovement2D.cs
@@ -6,6 +6,7 @@ public class CharacterMovement2D : MonoBehaviour
     public SpriteRenderer[] renderers;
     
     public float moveSpeed = 5f;
+    public float jumpPower = 10f;
     private float h;
 
     void Start()
@@ -18,6 +19,17 @@ public class CharacterMovement2D : MonoBehaviour
     {
         h = Input.GetAxis("Horizontal"); // A, D 키보드에 대한 입력값
 
+        ChangeSprite();
+        Jump();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void ChangeSprite()
+    {
         // if (h == 0) // 키를 누르지 않았을 때
         // {
         //     renderers[0].gameObject.SetActive(true);
@@ -32,26 +44,46 @@ public class CharacterMovement2D : MonoBehaviour
         renderers[0].gameObject.SetActive(h == 0);
         renderers[1].gameObject.SetActive(h != 0);
 
-        // if (h < 0) // 왼쪽 바라보는 상태
+        if (h < 0) // 왼쪽 바라보는 상태
+        {
+            // renderers[0].flipX = true;
+            // renderers[1].flipX = true;
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (h > 0)
+        {
+            // renderers[0].flipX = false;
+            // renderers[1].flipX = false;
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        
+        
+        // // 삼항 연산자 활용
+        // if (h != 0)
         // {
-        //     renderers[0].flipX = true;
-        //     renderers[1].flipX = true;
-        // }
-        // else if (h > 0)
-        // {
-        //     renderers[0].flipX = false;
-        //     renderers[1].flipX = false;
+        //     Vector3 scale = h < 0 ? new Vector3(-1, 1, 1) : new Vector3(1, 1, 1);
+        //
+        //     transform.localScale = scale;
         // }
 
-        if (h != 0)
-        {
-            renderers[0].flipX = h < 0;
-            renderers[1].flipX = h < 0;
-        }
+        // if (h != 0)
+        // {
+        //     renderers[0].flipX = h < 0;
+        //     renderers[1].flipX = h < 0;
+        //     renderers[2].flipX = h < 0;
+        // }
     }
 
-    void FixedUpdate()
+    private void Move()
     {
         characterRb.linearVelocityX = h * moveSpeed; // x축으로 속도를 변경하는 기능
+    }
+
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            characterRb.AddForceY(jumpPower, ForceMode2D.Impulse);
+        }
     }
 }
