618c2be2b900089092f45040e473f00f2c6a046b
JeongDay <cidd1332@gmail.com>
2025-12-19 13:50:32 +0900

    2025.12.19 금요일 - 오후시간 1
    하노이 타워 로직 적용

--- Assets/_Practice/02. Scripts/Hanoi/BoardStick.cs
+++ Assets/_Practice/02. Scripts/Hanoi/BoardStick.cs
@@ -3,13 +3,57 @@ using UnityEngine;
 
 public class BoardStick : MonoBehaviour
 {
-    public enum StickType { Left, Center, Right }
+    public enum StickType { Left = -7, Center = 0, Right = 7 }
     public StickType stickType;
-    
+
     public Stack<GameObject> stack = new Stack<GameObject>();
 
     void OnMouseDown()
     {
-        Debug.Log($"현재 클릭한 막대기 위치는 {stickType}입니다.");
+        if (!HanoiTower.isSelected) // 링 선택
+        {
+            HanoiTower.isSelected = true;
+
+            HanoiTower.selectedRing = stack.Pop();
+        }
+        else // 링 옮기기
+        {
+            if (stack.Count > 0)
+            {
+                GameObject peekRing = stack.Peek();
+                int peekNumber = peekRing.GetComponent<Ring>().ringNumber;
+
+                if (HanoiTower.selectedRing.GetComponent<Ring>().ringNumber > peekNumber)
+                {
+                    Debug.Log("<color=yellow>작은 링 위에 큰 링을 올릴 수 없습니다.</color>");
+                    return;
+                }
+            }
+            
+            stack.Push(HanoiTower.selectedRing);
+            
+            HanoiTower.selectedRing.GetComponent<Rigidbody>().linearVelocity = Vector3.zero;
+            HanoiTower.selectedRing.GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+            
+            HanoiTower.selectedRing.transform.position = new Vector3((int)stickType, 5f, 0);
+
+            HanoiTower.isSelected = false;
+            HanoiTower.selectedRing = null;
+        }
+    }
+
+    public void PopRing()
+    {
+        
+    }
+
+    public bool CheckRing()
+    {
+        
+    }
+
+    public void PushRing()
+    {
+        
     }
 }
--- Assets/_Practice/02. Scripts/Hanoi/HanoiTower.cs
+++ Assets/_Practice/02. Scripts/Hanoi/HanoiTower.cs
@@ -1,4 +1,6 @@
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class HanoiTower : MonoBehaviour
@@ -7,8 +9,16 @@ public class HanoiTower : MonoBehaviour
     public HanoiLevel hanoiLevel;
 
     [SerializeField] private GameObject[] ringPrefabs;
-    
+
     [SerializeField] private BoardStick[] sticks;
+
+    public List<GameObject> stackPreView1 = new List<GameObject>();
+    public List<GameObject> stackPreView2 = new List<GameObject>();
+    public List<GameObject> stackPreView3 = new List<GameObject>();
+
+
+    public static GameObject selectedRing;
+    public static bool isSelected;
 
     IEnumerator Start()
     {
@@ -17,10 +27,17 @@ public class HanoiTower : MonoBehaviour
             GameObject ring = Instantiate(ringPrefabs[i]);
 
             ring.transform.position = new Vector3(-7f, 5f, 0); // Left Stick 위에 생성
-            
+
             sticks[0].stack.Push(ring); // Left Stick의 스택 자료구조에 데이터 추가
-            
+
             yield return new WaitForSeconds(1f);
         }
+    }
+
+    void Update()
+    {
+        stackPreView1 = sticks[0].stack.ToList();
+        stackPreView2 = sticks[1].stack.ToList();
+        stackPreView3 = sticks[2].stack.ToList();
     }
 }
